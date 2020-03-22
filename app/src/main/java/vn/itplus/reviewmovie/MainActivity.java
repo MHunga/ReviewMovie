efd5ba6ec5f3de32213f1525cc3540594bdf9908
Manh Hung <hung1most1@gmail.com>
2020-03-22 19:58:54 +0700

    update

--- app/src/main/java/vn/itplus/reviewmovie/MainActivity.java
+++ app/src/main/java/vn/itplus/reviewmovie/MainActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MenuItem;
@@ -30,70 +36,44 @@ public class MainActivity extends AppCompatActivity  {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.ALPHA_CHANGED,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        loadFragment(new HomeFragment());
-
         BottomNavigationView navigationView = findViewById(R.id.navigation);
-        navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(navigationView, navController);
         CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) navigationView.getLayoutParams();
         layoutParams.setBehavior(new BottomNavigationBehavior() );
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment fragment;
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    fragment = new HomeFragment();
-                    loadFragment(fragment);
-
-                    return true;
-                case R.id.navigation_search:
-                    fragment = new SearchFragment();
-                    loadFragment(fragment);
-
-                    return true;
-                case R.id.navigation_notification:
-                    fragment = new NotificationFragment();
-                    loadFragment(fragment);
-
-                    return true;
-                case R.id.navigation_profile:
-                    fragment = new ProfileFragment();
-                    loadFragment(fragment);
-
-                    return true;
-
-            }
-
-            return false;
-        }
-    };
-
-    private void loadFragment(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_container, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
 
     @Override
     public void onBackPressed() {
-        if (doublePressToExit) {
-            super.onBackPressed();
-            return;
-        }
-        this.doublePressToExit = true;
-        Toast.makeText(MainActivity.this, getString(R.string.click_back_to_exit), Toast.LENGTH_LONG).show();
-        new Handler().postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                doublePressToExit = false;
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        int seletedItemId = bottomNavigationView.getSelectedItemId();
+        if (R.id.navigation_home != seletedItemId) {
+            setHomeItem(MainActivity.this);
+        } else {
+            if (doublePressToExit) {
+                super.onBackPressed();
+                return;
             }
-        }, 2000);
+            this.doublePressToExit = true;
+            Toast.makeText(MainActivity.this, getString(R.string.click_back_to_exit), Toast.LENGTH_LONG).show();
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    doublePressToExit = false;
+                }
+            }, 2000);
+
+        }
+
+
+    }
+
+    private void setHomeItem(Activity activity) {
+        BottomNavigationView bottomNavigationView =
+                activity.findViewById(R.id.navigation);
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
     }
 
 
