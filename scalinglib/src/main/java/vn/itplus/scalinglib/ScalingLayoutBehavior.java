483418bf050b70a60fe1c1765bd3df33cfd3152f
Manh Hung <hung1most1@gmail.com>
2020-04-15 14:13:53 +0700

    update

---
+++ scalinglib/src/main/java/vn/itplus/scalinglib/ScalingLayoutBehavior.java
@@ -0,0 +1,40 @@
+package vn.itplus.scalinglib;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.google.android.material.appbar.AppBarLayout;
+
+/**
+ * Created by mertsimsek on 30/09/2017.
+ */
+
+public class ScalingLayoutBehavior extends CoordinatorLayout.Behavior<ScalingLayout> {
+
+    private final float toolbarHeightInPixel;
+
+    public ScalingLayoutBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        toolbarHeightInPixel = context.getResources().getDimensionPixelSize(R.dimen.sl_toolbar_size);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, ScalingLayout child, View dependency) {
+        return dependency instanceof AppBarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, ScalingLayout child, View dependency) {
+        int totalScrollRange = ((AppBarLayout) dependency).getTotalScrollRange();
+        child.setProgress((-dependency.getY()) / totalScrollRange);
+        if (totalScrollRange + dependency.getY() > (float) child.getMeasuredHeight() / 2) {
+            child.setTranslationY(totalScrollRange + dependency.getY() + toolbarHeightInPixel - (float) child.getMeasuredHeight() / 2);
+        } else {
+            child.setTranslationY(toolbarHeightInPixel);
+        }
+        return super.onDependentViewChanged(parent, child, dependency);
+    }
+}
---
+++ scalinglib/src/main/java/vn/itplus/scalinglib/State.java
@@ -0,0 +1,9 @@
+package vn.itplus.scalinglib;
+
+/**
+ * Created by mertsimsek on 01/10/2017.
+ */
+
+public enum State {
+    EXPANDED, COLLAPSED, PROGRESSING
+}
